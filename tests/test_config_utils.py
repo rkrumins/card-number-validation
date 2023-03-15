ec5ebbf170b60f38404cbbf9ec0440dba69f2712
rkrumins <kruminsrinalds@gmail.com>
2023-03-15 18:25:25 +0000

    Added fix for the issue so that path to test_config.yml is resolved dynamically

--- tests/test_config_utils.py
+++ tests/test_config_utils.py
@@ -1,14 +1,15 @@
 from pytest import raises
 import credit_card_validator.utils.config_utils as object_under_test
 from credit_card_validator.exceptions import ConfigFileNotFoundException
-
+import os
+module_path = os.path.dirname(os.path.realpath(__file__))
 
 def test_should_load_valid_yaml_file_successfully():
-    test_config_file_path = "test_data/test_config.yml"
+    test_config_file_path = os.path.join(module_path, "test_data/test_config.yml")
     test_config_file_contents = object_under_test.load_config_from_local_filesystem(test_config_file_path)
     assert test_config_file_contents["MASK_UNMASKED_DIGITS_COUNT"] == 4
 
 def test_should_raise_file_not_found_exception_for_invalid_input_path():
-    test_config_file_path = "test_data/non_existant_path.yml"
+    test_config_file_path = os.path.join(module_path, "test_data/non_existant_path.yml")
     with raises(ConfigFileNotFoundException):
         object_under_test.load_config_from_local_filesystem(test_config_file_path)
